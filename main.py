be1609677f21cdf4d858225aee153f1f2c41a6d2
lokilanka <xw7771428@gmail.com>
2021-09-10 06:23:23 +0800

    fix ipc problem

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ def build_argparser():
     return paser
 
 
-def patch_preprocess(patch, img, mapping, inverse_mapping, opts):
-    for r in range(img.shape[0]):
+def patch_preprocess(patch, img, i, mapping, inverse_mapping, opts):
+    for r in range(i * img.shape[0] // opts.workers, (i + 1) * img.shape[0] // opts.workers):
         for c in range(img.shape[1]):
             sub_img = read_img(mapping[find_closest(img[r, c], inverse_mapping)])
             patch[r * opts.scale:(r + 1) * opts.scale, c * opts.scale:(c + 1) * opts.scale, :] = cv2.resize(sub_img,
                                                                                                             (opts.scale,
                                                                                                              opts.scale))
-
+    return patch
 
 def main():
     parser = build_argparser()
@@ -31,31 +31,29 @@ def main():
 
     mapping_from_video(opts.video, opts.mapping)
 
-    with open('test.json', 'r') as f:
+    with open(opts.mapping, 'r') as f:
         mappings = json.load(f)
     inverse_mapping = []
     for key in mappings.keys():
         inverse_mapping.append([int(x) for x in key.split('-')])
     inverse_mapping = np.array(inverse_mapping)
 
-    videoWriter = cv2.VideoWriter('out.mp4', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 24, size)
+    videoWriter = cv2.VideoWriter('out.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, size)
 
     imgs = os.listdir(directory)
-    cur = 0
+
     for img in tqdm.tqdm(imgs):
         img = read_img(os.path.join(directory, img))
         res = np.zeros_like(img)
         img = cv2.resize(img, (img.shape[1] // opts.scale, img.shape[0] // opts.scale))
-        pool = [mp.Process(target=patch_preprocess,
-                           args=(res[i * res.shape[0] // opts.workers:(i + 1) * res.shape[0] // opts.workers, :, :],
-                                 img[i * img.shape[0] // opts.workers:(i + 1) * img.shape[0] // opts.workers, :, :],
-                                 mappings, inverse_mapping, opts))
-                for i in range(opts.workers)]
-        for p in pool:
-            p.start()
-        for p in pool:
-            p.join()
+
+        pool = mp.Pool(processes=opts.workers)
+        results = [pool.apply_async(patch_preprocess, [res, img, i, mappings, inverse_mapping, opts]) for i in range(opts.workers)]
+        output = np.array([res.get() for res in results])
+        res = np.sum(output, axis=0).astype(np.uint8)
+
         videoWriter.write(res)
+        cv2.imwrite('./tmp.jpg', res)
     videoWriter.release()
 
 
